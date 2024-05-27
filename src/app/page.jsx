44290f8d2dffec3009e6ce0d6264b9816761fdda
NeiJunio <neijunio50@gmail.com>
2024-05-26 22:41:27 -0300

    alterando grid layout - clientes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -64,58 +64,59 @@ export default function Home() {
 
                         <input type="hidden" id="clienteId" className={styles.input} />
 
-                        <div className={styles.input_row}>
-                            <div className={`${styles.input_wrapper} ${styles.input_small}`}>
+                        <div className={styles.grid}>
+                            <div className={`${styles.grid_item} ${styles.grid_item_small}`}>
                                 <label for="codigo" className={styles.label}>Código</label>
                                 <input type="number" id="codigo" name="codigo" required className={styles.input} />
                             </div>
 
-                            <div className={`${styles.input_wrapper} ${styles.input_large}`}>
+                            <div className={`${styles.grid_item} ${styles.grid_item_large}`}>
                                 <label for="nome" className={styles.label}>Nome:</label>
                                 <input type="text" id="nome" name="nome" required className={styles.input} />
                             </div>
 
-                            <div className={`${styles.input_wrapper} ${styles.input_medium}`}>
+                            <div className={`${styles.grid_item} ${styles.grid_item_medium}`}>
                                 <label for="cpf" className={styles.label}>CPF:</label>
                                 <input type="text" id="cpf" name="cpf" required className={styles.input} />
                             </div>
                         </div>
 
-                        <div className={styles.input_row}>
-                            <div className={`${styles.input_wrapper} ${styles.input_small}`}>
-                                <label for="data" className={styles.label}>Data de nascimento:</label>
-                                <input type="date" id="data" name="data" required className={styles.input} />
-                            </div>
 
-                            <div className={`${styles.input_wrapper} ${styles.input_small}`}>
-                                <label for="sexo" className={styles.label}>Sexo:</label>
-                                <select id="sexo" name="sexo" required className={`${styles.select} ${styles.input_small_select}`}>
-                                    <option value="">Selecionar</option>
-                                    <option value="masculino">Masculino</option>
-                                    <option value="feminino">Feminino</option>
-                                </select>
-                            </div>
-
-                            <div className={`${styles.input_wrapper} ${styles.input_large}`}>
-                                <label for="email" className={styles.label}>Email:</label>
-                                <input type="email" id="email" name="email" required className={styles.input} />
-                            </div>
-
-                            <div className={`${styles.input_wrapper} ${styles.input_medium}`}>
-                                <label for="telefone" className={styles.label}>Telefone:</label>
-                                <input type="tel" id="telefone" name="telefone" required className={styles.input} />
-                            </div>
+                        <div className={`${styles.grid_item} ${styles.grid_item_small}`}>
+                            <label for="data" className={styles.label}>Data de nascimento:</label>
+                            <input type="date" id="data" name="data" required className={styles.input} />
                         </div>
 
-                        <div className={styles.input_row}>
-                            <div className={`${styles.input_wrapper} ${styles.input_large}`}>
+                        <div className={`${styles.grid_item} ${styles.grid_item_small}`}>
+                            <label for="sexo" className={styles.label}>Sexo:</label>
+                            <select id="sexo" name="sexo" required className={`${styles.select} ${styles.input_small_select}`}>
+                                <option value="">Selecionar</option>
+                                <option value="masculino">Masculino</option>
+                                <option value="feminino">Feminino</option>
+                            </select>
+                        </div>
+
+                        <div className={`${styles.grid_item} ${styles.grid_item_large}`}>
+                            <label for="email" className={styles.label}>Email:</label>
+                            <input type="email" id="email" name="email" required className={styles.input} />
+                        </div>
+
+                        <div className={`${styles.grid_item} ${styles.grid_item_small}`}>
+                            <label for="telefone" className={styles.label}>Telefone:</label>
+                            <input type="tel" id="telefone" name="telefone" required className={styles.input} />
+                        </div>
+
+
+                       
+                        <div className={`${styles.grid_item} ${styles.grid_item_small}`}>
                                 <label for="access-level" className={styles.label}>Nível de Acesso:</label>
-                                <select id="access-level" name="access-level" className={`${styles.select} ${styles.input_small_select}`}>
+                                <select id="access-level" name="access-level"
+                                className={`${styles.select} ${styles.grid_item_small}`}>
                                     <option value="user" className={styles.option}>Usuário</option>
                                     <option value="admin" className={styles.option}>Administrador</option>
                                 </select>
                             </div>
-                        </div>
+                       
 
                         <div className={styles.footer_form}>
                             <button type="button" onclick="cancelarAcao()" className={styles.button_cancel}>Cancelar</button>
@@ -123,7 +124,7 @@ export default function Home() {
                         </div>
                     </form>
 
-                    
+
                     {/* <table id="tabelaClientes" className={styles.table}>
                         <thead>
                             <tr>
